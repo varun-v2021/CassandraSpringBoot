4ffb224d8be04478a16072ee89e5c96b0cb784aa
Varun <varun.v2021@gmail.com>
2021-05-13 17:38:00 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/SpringCassandraSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCassandraSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCassandraSampleApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/repository/TutorialRepository.java
@@ -0,0 +1,20 @@
+/**
+ * @author Varun
+ *
+ */
+package com.example.demo.repository;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.cassandra.repository.AllowFiltering;
+import org.springframework.data.cassandra.repository.CassandraRepository;
+
+import com.example.demo.model.Tutorial;
+
+public interface TutorialRepository extends CassandraRepository<Tutorial, UUID> {
+  @AllowFiltering
+  List<Tutorial> findByPublished(boolean published);
+  
+  List<Tutorial> findByTitleContaining(String title);
+}
